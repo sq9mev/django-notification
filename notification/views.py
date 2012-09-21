c8f0a40f20b86e5b736bd897c41fa2cd645311ee
Konrad Rotkiewicz <konrad.rotkiewicz@stxnext.pl>
2012-09-21 14:27:51 +0200

    django1.4 fix

--- notification/views.py
+++ notification/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponseRedirect, Http404
 from django.template import RequestContext
 
 from django.contrib.auth.decorators import login_required
-from django.contrib.syndication.views import feed
+from django.contrib.syndication.views import Feed
 
 from notification.models import *
 from notification.decorators import basic_auth_required, simple_basic_auth_callback
